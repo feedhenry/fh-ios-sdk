30a72bf61f96772c042719b680f14eff2d5a63d7
Wei Li <wei.li@feedhenry.com>
2014-05-08 13:42:02 +0100

    fix an issue with dist files

--- FH/build.sh
+++ FH/build.sh
@@ -15,6 +15,7 @@ $XC_BUILD VALID_ARCHS="i386 armv7 armv7s arm64 x86_64" -project "$BUILD_PROJECT_
 if [ "$?" != "0" ]; then echo >&2 "Error: xcodebuild failed"; exit 1; fi
 
 FH_SDK_VERSION=`head -1 VERSION.txt`
+rm -rf "$DIST_DIR"
 if [ ! -d $DIST_DIR ]; then
   mkdir "$DIST_DIR"
 fi
