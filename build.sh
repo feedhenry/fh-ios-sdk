cf67adbb1cc16a6bf9e990b69f943dd0f10a43cb
Christos Vasilakis <cvasilak@gmail.com>
2015-02-26 17:49:18 +0200

    adjust build.sh script after sdk/demos polish

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 BUILD_DIR="./build"
 DIST_DIR="./dist"
 rm -rf "$BUILD_DIR"
-rm -rf "DIST_DIR/*.zip"
+rm -rf "$DIST_DIR/*.zip"
 
-BUILD_PROJECT_NAME="FH.xcodeproj"
+BUILD_PROJECT_NAME="fh-ios-sdk.xcodeproj"
 BUILD_TARGET_NAME="framework"
 BUILD_CONFIGURATION="Release"
 
@@ -19,14 +19,17 @@ rm -rf "$DIST_DIR"
 if [ ! -d $DIST_DIR ]; then
   mkdir "$DIST_DIR"
 fi
+
 cd "build/Release-iphoneos/"
 zip -9ry "../../$DIST_DIR/fh-framework-$FH_SDK_VERSION.zip" "FH.framework"
 
-rm -rf "../../../FHStarterProject/FH.framework"
-cp -R "FH.framework" "../../../FHStarterProject/"
-cd "../../../"
-rm -rf "FHStarterProject/FHStarterProject.xcodeproj/xcuserdata"
-rm -rf "FHStarterProject/FHStarterProject.xcodeproj/project.xcworkspace"
-rm -rf "FHStarterProject/build"
-zip -9ry "FH/dist/fh-starter-project-$FH_SDK_VERSION.zip" "FHStarterProject"
+# update 'starter project' with the new framework build
+cd "../../demos/FHStarterProject/"
+rm -rf "FH.framework"
+cp -R "../../build/Release-iphoneos/FH.framework" "."
+rm -rf "FHStarterProject.xcodeproj/xcuserdata"
+rm -rf "FHStarterProject.xcodeproj/project.xcworkspace"
+rm -rf "build"
+zip -9ry "../../$DIST_DIR/fh-starter-project-$FH_SDK_VERSION.zip" "."
+
 
