e036e262bf7e88a4cba194366d4fd26121704f89
Christos Vasilakis <cvasilak@gmail.com>
2015-04-06 17:47:21 +0300

    missed typo

--- Pods/ASIHTTPRequest/Classes/ASIAuthenticationDialog.h
+++ Pods/ASIHTTPRequest/Classes/ASIAuthenticationDialog.h
@@ -6,8 +6,8 @@
 //  Copyright 2009 All-Seeing Interactive. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
 @class ASIHTTPRequest;
 
 typedef enum _ASIAuthenticationType {
