0972c56483f39bdb75ef03afc2558bcb75f48c8c
corinne krych <corinnekrych@gmail.com>
2015-11-20 09:41:20 +0100

    Update header, LICENSE and licenses.txt

--- fh-ios-sdkTests/FHCloudRequestTest.m
+++ fh-ios-sdkTests/FHCloudRequestTest.m
@@ -1,10 +1,19 @@
-//
-//  FHcloudRequest.m
-//  fh-ios-sdk
-//
-//  Created by Corinne Krych on 04/11/15.
-//  Copyright © 2015 FeedHenry. All rights reserved.
-//
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <XCTest/XCTest.h>
 #import "FHCloudRequest.h"
